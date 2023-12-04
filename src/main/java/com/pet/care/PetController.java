dbc59445513019c83d31a582e6551a624e628bdb
DeveloperSunbe <dufmaskfk12@gmail.com>
2023-12-04 14:45:19 +0900

    유저 별로 반려동물 보이게 하는거 해결

--- src/main/java/com/pet/care/PetController.java
+++ src/main/java/com/pet/care/PetController.java
@@ -2,12 +2,16 @@ package com.pet.care;
 
 import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import dao.PetDAO;
 import vo.PetVO;
+import vo.UserVO;
 
 @Controller
 public class PetController {
@@ -17,6 +21,9 @@ public class PetController {
 	
 	PetDAO pet_dao;
 	
+	@Autowired
+	HttpServletRequest request;
+	
 	public PetController(PetDAO pet_dao) {
 		this.pet_dao = pet_dao;
 		System.out.println("petController 생성자");
@@ -25,7 +32,8 @@ public class PetController {
 	// 펫 정보 메인 화면 펫 리스트 보임
 	@RequestMapping(value={"petinfo_main.do"})
 	public String list(Model model) {
-		List<PetVO> list = pet_dao.selectList();
+		int idx = (((UserVO)request.getSession().getAttribute("id")).getU_idx());
+		List<PetVO> list = pet_dao.selectList(idx);
 		
 		model.addAttribute("list", list);		
 		return VIEW_PATH + "petinfo_main.jsp";
